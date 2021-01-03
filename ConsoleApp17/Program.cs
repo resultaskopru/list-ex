74ea1d9b1cd0a970b170701950796a22d0e553da
Resul Taşköprü <resultaskpru@gmail.com>
2021-01-03 15:57:20 +0300

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> gs1 = new List<int>();
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                gs1.Add(r.Next(100));
+            }
+            foreach (int item in gs1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("////////////////////////");
+            for (int i = 0; i < 5; i++)
+            {
+                int gd = gs1[i];
+                gs1[i] = gs1[gs1.Count - 1 - i];
+                gs1[gs1.Count - 1 - i] = gd;
+            }
+            foreach (var item in gs1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
